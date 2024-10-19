a9f7e58b67214d1f1c414a5e950882ff98fdf1ad
Yared <yaredhabtamu08@gmail.com>
2024-10-19 18:13:07 +0300

    Done

---
+++ alx_book_store.sql
@@ -0,0 +1,24 @@
+CREATE TABLE Books(
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130),
+    FOREIGN KEY author_id REFERENCES Authors(author_id),
+    price FLOAT,
+    publication_date DATE   
+);
+
+CREATE TABLE Authors(
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+CREATE TABLE Customers(
+    customer_id INT Primary Key,
+    customer_name VARCHAR(213)
+ 
+);
+CREATE TABLE Books(
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130),
+    FOREIGN KEY author_id REFERENCES Authors(author_id),
+    price FLOAT,
+    publication_date DATE   
+);
