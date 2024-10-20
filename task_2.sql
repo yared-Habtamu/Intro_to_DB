3b6b4509394eff657d0465acd6bef2dc7b253284
Yared <149290215+yared-Habtamu@users.noreply.github.com>
2024-10-20 18:16:21 +0300

    Corrected

--- task_2.sql
+++ task_2.sql
@@ -26,7 +26,7 @@ CREATE TABLE Orders(
 
 CREATE TABLE Order_Details(
    order_detailid PRIMARY KEY,
-   FOREIGN KEY order_id REFERENCES Orders(order_id),
-   FOREIGN KEY book_id REFERENCES Books(book_id),
+   FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+   FOREIGN KEY (book_id) REFERENCES Books(book_id)
    quantity DOUBLE
 )
