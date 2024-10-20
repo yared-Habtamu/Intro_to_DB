0f3a78ba8fde17534cb2cacdf651f52182568fd8
Yared <149290215+yared-Habtamu@users.noreply.github.com>
2024-10-20 20:12:45 +0300

    Done

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,11 @@
+-- USE alx_book_store;
+
+-- SELECT COLUMN_NAME, COLUMN_TYPE
+-- FROM INFORMATION_SCHEMA.COLUMNS
+-- WHERE TABLE_NAME='Books' AND TABLE_SCHEMA ='alx_book_store';
+
 USE alx_book_store;
 
-SELECT COLUMN_NAME, COLUMN_TYPE
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME='Books' AND TABLE_SCHEMA ='alx_book_store';
+SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM information_schema.columns
+WHERE table_name = 'books' AND table_schema = 'alx_book_store';
