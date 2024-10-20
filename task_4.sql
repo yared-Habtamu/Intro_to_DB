8f8618f5d0edd8831e0f39894f6d5705c7f7cc7f
yared-Habtamu <yaredhabtamu08@gmail.com>
2024-10-20 20:49:49 +0300

    Done

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,5 @@
+USE alx_book_store;
 
+SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM information_schema.columns
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
